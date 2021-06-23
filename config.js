cdb196b7d6b0f3233a0d97f997845c23cd16cdd3
avneet1604 <avneet.devgan@gmail.com>
2021-06-23 14:42:07 +0530

    created google sign in

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAULW3ou1tHUpWE5xe6S1o6HIxvv5gbDu0",
+    authDomain: "spectagram-app-26dcd.firebaseapp.com",
+    projectId: "spectagram-app-26dcd",
+    storageBucket: "spectagram-app-26dcd.appspot.com",
+    messagingSenderId: "928005179813",
+    appId: "1:928005179813:web:89d303f11ae1f9054ac04c"
+  };
