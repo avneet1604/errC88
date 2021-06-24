0be43d585579684ea170858251235aa08010838b
avneet1604 <avneet.devgan@gmail.com>
2021-06-24 22:31:42 +0530

    created logout functionality with profile screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,42 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  Dimensions
+} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { RFValue } from "react-native-responsive-fontsize";
 import * as Google from "expo-google-app-auth";
 import firebase from "firebase";
 
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
 export default class LoginScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
   isUserEqual = (googleUser, firebaseUser) => {
     if (firebaseUser) {
       var providerData = firebaseUser.providerData;
@@ -91,21 +124,83 @@ export default class LoginScreen extends Component {
   };
 
   render() {
+      if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
     return (
       <View style={styles.container}>
-        <Button
-          title="Sign in with Google"
-          onPress={() => this.signInWithGoogleAsync()}
-        ></Button>
+      <SafeAreaView style={styles.droidSafeArea} />
+      <View style={styles.appTitle}>
+        <Image
+          source={require("../assets/logo.png")}
+          style={styles.appIcon}
+        ></Image>
+        <Text style={styles.appTitleText}>{`Spectagram\nApp`}</Text>
       </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => this.signInWithGoogleAsync()}
+        >
+          <Image
+            source={require("../assets/google_icon.png")}
+            style={styles.googleIcon}
+          ></Image>
+          <Text style={styles.googleText}>Sign in with Google</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
     );
   }
+}
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#15193c"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+  },
+  appTitle: {
+    flex: 0.4,
     justifyContent: "center",
     alignItems: "center"
+  },
+  appIcon: {
+    width: RFValue(130),
+    height: RFValue(130),
+    resizeMode: "contain"
+  },
+  appTitleText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: RFValue(40),
+    fontFamily: "Bubblegum-Sans"
+  },
+  buttonContainer: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    width: RFValue(250),
+    height: RFValue(50),
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    borderRadius: RFValue(30),
+    backgroundColor: "white"
+  },
+  googleIcon: {
+    width: RFValue(30),
+    height: RFValue(30),
+    resizeMode: "contain"
+  },
+  googleText: {
+    color: "black",
+    fontSize: RFValue(20),
+    fontFamily: "Bubblegum-Sans"
   }
 });
